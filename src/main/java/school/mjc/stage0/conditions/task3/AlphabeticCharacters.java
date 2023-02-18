45518a4503f21b6c40173d2ff4fc8244f8767125
wickerman2019 <sverige@i.ua>
2023-02-18 17:46:12 +0200

    Fix AlphabeticCharacters bug

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -3,8 +3,10 @@ package school.mjc.stage0.conditions.task3;
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         if((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')){
-            if ((character == 'a') || (character == 'i') (character ==e  character ==o character ==u') ||
-            (character == 'a i e o u') )
+            if ((character == 'a') || (character == 'i') || (character == 'e') || (character == 'o') || (character == 'u') ||
+            (character == 'A') || (character == 'I') || (character == 'E') || (character == 'O') || (character == 'U')) 
+            System.out.println("Vowel");
+            else System.out.println("Consonant");
         }
         else
             System.out.println("wrong alphabet!");
