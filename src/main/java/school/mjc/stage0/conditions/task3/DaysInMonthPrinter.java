dbe430479d76f53e7f2c1e10480e79f21e14a000
wickerman2019 <sverige@i.ua>
2023-02-18 19:41:45 +0200

    Fix the bug day in month

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -5,8 +5,8 @@ public class DaysInMonthPrinter {
         if((month < 1) || (month > 12)) System.out.println("wrong number!");
         else {
             if(month==2) System.out.println(28);
-            if((month==1) && (month==3) && (month==5) && (month==7) && (month==8) && (month==10) && (month==12)) System.out.println(31);
-            if((month==4) && (month==6) && (month==9) && (month==11)) System.out.println(30);
+            if((month==1) || (month==3) || (month==5) || (month==7) || (month==8) || (month==10) || (month==12)) System.out.println(31);
+            if((month==4) || (month==6) || (month==9) || (month==11)) System.out.println(30);
         }
     }
 }
